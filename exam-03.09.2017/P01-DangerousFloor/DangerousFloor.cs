11f7de31dd0690907d3fffd6f5533563d000451e
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-02-19 16:59:41 +0200

    Added new exercises

---
+++ exam-03.09.2017/P01-DangerousFloor/DangerousFloor.cs
@@ -0,0 +1,170 @@
+﻿namespace P01_DangerousFloor
+{
+    using System;
+    using System.Linq;
+
+    public class DangerousFloor
+    {
+        public static void Main()
+        {
+            var board = new char[8, 8];
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                var elements = Console.ReadLine().Split(',').Select(char.Parse).ToArray();
+
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    board[row, col] = elements[col];
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                char piece = command[0];
+                int currentRow = (int)char.GetNumericValue(command[1]);
+                int currentCol = (int)char.GetNumericValue(command[2]);
+                int nextRow = (int)char.GetNumericValue(command[4]);
+                int nextCol = (int)char.GetNumericValue(command[5]);
+
+                if (board[currentRow, currentCol] != piece)
+                {
+                    Console.WriteLine("There is no such a piece!");
+                    continue;
+                }
+
+                switch (piece)
+                {
+                    case 'K':
+                        if (!CheckKing(currentRow, currentCol, nextRow, nextCol))
+                        {
+                            Console.WriteLine($"Invalid move!");
+                            continue;
+                        }
+
+                        break;
+
+                    case 'R':
+                        if (!CheckRock(currentRow, currentCol, nextRow, nextCol))
+                        {
+                            Console.WriteLine($"Invalid move!");
+                            continue;
+                        }
+
+                        break;
+
+                    case 'B':
+                        if (!CheckBishop(currentRow, currentCol, nextRow, nextCol))
+                        {
+                            Console.WriteLine($"Invalid move!");
+                            continue;
+                        }
+
+                        break;
+
+                    case 'Q':
+                        if (!CheckQueen(currentRow, currentCol, nextRow, nextCol))
+                        {
+                            Console.WriteLine($"Invalid move!");
+                            continue;
+                        }
+
+                        break;
+
+                    case 'P':
+                        if (!CheckPawn(currentRow, currentCol, nextRow, nextCol))
+                        {
+                            Console.WriteLine($"Invalid move!");
+                            continue;
+                        }
+
+                        break;
+                }
+
+                if (!ValidCoordinates(currentRow, currentCol, nextRow, nextCol))
+                {
+                    Console.WriteLine($"Move go out of board!");
+                    continue;
+                }
+
+                board[nextRow, nextCol] = piece;
+                board[currentRow, currentCol] = 'x';
+            }
+        }
+
+        public static bool CheckKing(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (Math.Abs(currentRow - nextRow) == Math.Abs(currentCol - nextCol)
+                || (currentRow == nextRow && Math.Abs(currentCol - nextCol) <= 1)
+                || (currentCol == nextCol && Math.Abs(currentRow - nextRow) <= 1))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckRock(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (nextRow == currentRow || nextCol == currentCol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckBishop(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (Math.Abs(currentRow - nextRow) == Math.Abs(currentCol - nextCol))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckQueen(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (Math.Abs(currentRow - nextRow) == Math.Abs(currentCol - nextCol)
+                || currentRow == nextRow
+                || currentCol == nextCol)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckPawn(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (nextCol == currentCol && nextRow == currentRow - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ValidCoordinates(int currentRow, int currentCol, int nextRow, int nextCol)
+        {
+            if (nextRow < 0
+                || nextRow >= 8
+                || nextCol < 0
+                || nextCol >= 8)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ exam-03.09.2017/P03-GreedyTimes/GreedyTimes.cs
@@ -0,0 +1,97 @@
+﻿namespace P03_GreedyTimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class GreedyTimes
+    {
+        public static void Main()
+        {
+            long bagCapacity = long.Parse(Console.ReadLine());
+            var allTypes = new Dictionary<string, Dictionary<string, long>>();
+            allTypes["Gold"] = new Dictionary<string, long>();
+            allTypes["Gem"] = new Dictionary<string, long>();
+            allTypes["Cash"] = new Dictionary<string, long>();
+            var summedQuantity = new Dictionary<string, long>();
+            summedQuantity["Gold"] = 0;
+            summedQuantity["Gem"] = 0;
+            summedQuantity["Cash"] = 0;
+
+            string input = Console.ReadLine();
+            string pattern = @"(gold|[a-zA-Z]{3}|[a-zA-Z]+gem)[\s]+([0-9]+)";
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matchedTypes = regex.Matches(input);
+
+            foreach (Match match in matchedTypes)
+            {
+                string item = match.Groups[1].Value;
+                long quantity = long.Parse(match.Groups[2].Value);
+
+                if (bagCapacity >= quantity)
+                {
+                    if (item.ToLower() == "gold")
+                    {
+                        if (!allTypes["Gold"].ContainsKey(item))
+                        {
+                            allTypes["Gold"][item] = 0;
+                        }
+
+                        allTypes["Gold"][item] += quantity;
+                        summedQuantity["Gold"] += quantity;
+                        bagCapacity -= quantity;
+                    }
+
+                    else if (item.ToLower().EndsWith("gem") && item.Length > 3)
+                    {
+                        if (summedQuantity["Gem"] + quantity > summedQuantity["Gold"])
+                        {
+                            continue;
+                        }
+
+                        if (!allTypes["Gem"].ContainsKey(item))
+                        {
+                            allTypes["Gem"][item] = 0;
+                        }
+
+                        allTypes["Gem"][item] += quantity;
+                        summedQuantity["Gem"] += quantity;
+                        bagCapacity -= quantity;
+                    }
+
+                    else if (item.Length == 3)
+                    {
+                        if (summedQuantity["Cash"] + quantity > summedQuantity["Gem"])
+                        {
+                            continue;
+                        }
+
+                        if (!allTypes["Cash"].ContainsKey(item))
+                        {
+                            allTypes["Cash"][item] = 0;
+                        }
+
+                        allTypes["Cash"][item] += quantity;
+                        summedQuantity["Cash"] += quantity;
+                        bagCapacity -= quantity;
+                    }
+                }
+            }
+
+            foreach (var type in allTypes.Where(t => t.Value.Any()))
+            {
+                Console.WriteLine($"<{type.Key}> ${summedQuantity[type.Key]}");
+
+                var orderedItems = allTypes[type.Key]
+                    .OrderByDescending(i => i.Key)
+                    .ThenBy(i => i.Value);
+
+                foreach (var item in orderedItems)
+                {
+                    Console.WriteLine($"##{item.Key} - {item.Value}");
+                }
+            }
+        }
+    }
+}
