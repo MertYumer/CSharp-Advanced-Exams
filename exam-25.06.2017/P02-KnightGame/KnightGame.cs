70f3b3f4a4068523cf046d215a4d51fc955f5779
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-02-20 18:28:22 +0200

    Added new exercises

---
+++ exam-25.06.2017/P02-KnightGame/KnightGame.cs
@@ -0,0 +1,132 @@
+﻿namespace P02_KnightGame
+{
+    using System;
+
+    public class KnightGame
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var field = new char[n, n];
+            int bestKnightRow = -1;
+            int bestKnightCol = -1;
+            int mostAttackedKnights = 0;
+            int removedKnights = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                var elements = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    field[row, col] = elements[col];
+                }
+            }
+
+            while (true)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (field[row, col] == 'K')
+                        {
+                            int attackedKnights = CheckAttackedKnights(field, row, col);
+
+                            if (attackedKnights > mostAttackedKnights)
+                            {
+                                mostAttackedKnights = attackedKnights;
+                                bestKnightRow = row;
+                                bestKnightCol = col;
+                            }
+                        }
+                    }
+                }
+
+                if (mostAttackedKnights > 0)
+                {
+                    field[bestKnightRow, bestKnightCol] = '0';
+                    mostAttackedKnights = 0;
+                    removedKnights++;
+                }
+
+                else
+                {
+                    Console.WriteLine(removedKnights);
+                    return;
+                }
+            }
+        }
+
+        public static int CheckAttackedKnights(char[,] field, int knightRow, int knightCol)
+        {
+            int attackedKnights = 0;
+
+            if (knightRow - 1 >= 0 && knightCol - 2 >= 0)
+            {
+                if (field[knightRow - 1, knightCol - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow + 1 < field.GetLength(0) && knightCol - 2 >= 0)
+            {
+                if (field[knightRow + 1, knightCol - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow - 2 >= 0 && knightCol - 1 >= 0)
+            {
+                if (field[knightRow - 2, knightCol - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow - 2 >= 0 && knightCol + 1 < field.GetLength(1))
+            {
+                if (field[knightRow - 2, knightCol + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow - 1 >= 0 && knightCol + 2 < field.GetLength(1))
+            {
+                if (field[knightRow - 1, knightCol + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow + 1 < field.GetLength(0) && knightCol + 2 < field.GetLength(1))
+            {
+                if (field[knightRow + 1, knightCol + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow + 2 < field.GetLength(0) && knightCol - 1 >= 0)
+            {
+                if (field[knightRow + 2, knightCol - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (knightRow + 2 < field.GetLength(0) && knightCol + 1 < field.GetLength(1))
+            {
+                if (field[knightRow + 2, knightCol + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            return attackedKnights;
+        }
+    }
+}
---
+++ exam-25.06.2017/P03-NumberWars/NumberWars.cs
@@ -0,0 +1,116 @@
+﻿namespace P03_NumberWars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NumberWars
+    {
+        public static void Main()
+        {
+            var firstLine = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var secondLine = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var firstPlayer = new Queue<string>(firstLine);
+            var secondPlayer = new Queue<string>(secondLine);
+            int turns = 0;
+            bool gameOver = false;
+
+            while (turns < 1000000 && gameOver == false)
+            {
+                if (firstPlayer.Count == 0 || secondPlayer.Count == 0)
+                {
+                    break;
+                }
+
+                turns++;
+                string firstPlayerCard = firstPlayer.Dequeue();
+                string secondPlayerCard = secondPlayer.Dequeue();
+
+                int firstCardValue = GetNumberValue(firstPlayerCard);
+                int secondCardValue = GetNumberValue(secondPlayerCard);
+
+                if (firstCardValue > secondCardValue)
+                {
+                    firstPlayer.Enqueue(firstPlayerCard);
+                    firstPlayer.Enqueue(secondPlayerCard);
+                }
+
+                else if (firstCardValue < secondCardValue)
+                {
+                    secondPlayer.Enqueue(secondPlayerCard);
+                    secondPlayer.Enqueue(firstPlayerCard);
+                }
+
+                else
+                {
+                    var cardsOnTable = new List<string> { firstPlayerCard, secondPlayerCard };
+
+                    while (gameOver == false)
+                    {
+                        if (firstPlayer.Count < 3 && secondPlayer.Count < 3)
+                        {
+                            gameOver = true;
+                            break;
+                        }
+
+                        int firstSum = 0;
+                        int secondSum = 0;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            firstSum += firstPlayer.Peek().Last();
+                            secondSum += secondPlayer.Peek().Last();
+
+                            cardsOnTable.Add(firstPlayer.Dequeue());
+                            cardsOnTable.Add(secondPlayer.Dequeue());
+                        }
+
+                        if (firstSum > secondSum)
+                        {
+                            AddWonCards(cardsOnTable, firstPlayer);
+                            break;
+                        }
+
+                        else if (firstSum < secondSum)
+                        {
+                            AddWonCards(cardsOnTable, secondPlayer);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (firstPlayer.Count != secondPlayer.Count)
+            {
+                Console.WriteLine($"{(firstPlayer.Count > secondPlayer.Count ? "First" : "Second")} " +
+                    $"player wins after {turns} turns");
+            }
+
+            else
+            {
+                Console.WriteLine($"Draw after {turns} turns");
+            }
+        }
+
+        public static int GetNumberValue(string playerCard)
+        {
+            return int.Parse(playerCard.Substring(0, playerCard.Length - 1));
+        }
+
+        public static void AddWonCards(List<string> cardsOnTable, Queue<string> player)
+        {
+            var orderedCards = cardsOnTable
+                                .OrderByDescending(c => GetNumberValue(c))
+                                .ThenByDescending(c => c.Last());
+
+            foreach (var card in orderedCards)
+            {
+                player.Enqueue(card);
+            }
+        }
+    }
+}
